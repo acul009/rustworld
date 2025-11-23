7610daee0922e3da164482b8436bea026198eda4
L. Wlcek <luca@it-rahn.de>
2025-11-23 06:13:15 +0100

    first working simulation

---
+++ src/main.rs
@@ -0,0 +1,436 @@
+use std::{collections::HashMap, ops::Add, slice::Windows, sync::Arc};
+
+use crate::neural_network::{Action, Location, NeuralNetwork, NeuralTick};
+use rayon::prelude::*;
+
+pub mod neural_network;
+
+fn main() {
+    let settings = WorldSettings {
+        creature_generation_rate: 3,
+        food_regen_rate: 12,
+    };
+
+    let mut world = World::new(1000, 1000, Tile::Lava, settings);
+    println!("World created!");
+
+    loop {
+        for _ in 0..100 {
+            world.tick();
+        }
+        println!(
+            r#"
+            =========={}==========
+            Creatures: {}
+            "#,
+            world.current_tick,
+            world.creatures.len()
+        )
+    }
+}
+
+#[derive(Hash, Debug, PartialEq, Eq, Clone)]
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
+impl Position {
+    fn north(&self, amount: usize) -> Self {
+        Self {
+            x: self.x,
+            y: self.y.saturating_sub(amount),
+        }
+    }
+
+    fn south(&self, amount: usize) -> Self {
+        Self {
+            x: self.x,
+            y: self.y.saturating_add(amount),
+        }
+    }
+
+    fn east(&self, amount: usize) -> Self {
+        Self {
+            x: self.x.saturating_add(amount),
+            y: self.y,
+        }
+    }
+
+    fn west(&self, amount: usize) -> Self {
+        Self {
+            x: self.x.saturating_sub(amount),
+            y: self.y,
+        }
+    }
+
+    fn cardinal(&self, location: CardinalDirection, amount: usize) -> Self {
+        match location {
+            CardinalDirection::North => self.north(amount),
+            CardinalDirection::South => self.south(amount),
+            CardinalDirection::East => self.east(amount),
+            CardinalDirection::West => self.west(amount),
+        }
+    }
+
+    fn randomize(width: usize, height: usize) -> Self {
+        Self {
+            x: fastrand::usize(0..width),
+            y: fastrand::usize(0..height),
+        }
+    }
+}
+
+impl Add for Position {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x.saturating_add(rhs.x),
+            y: self.y.saturating_add(rhs.y),
+        }
+    }
+}
+
+pub struct World {
+    width: usize,
+    height: usize,
+    tiles: Vec<Tile>,
+    creatures: HashMap<Position, Creature>,
+    current_tick: u64,
+    settings: WorldSettings,
+}
+
+#[derive(Clone)]
+pub struct WorldSettings {
+    food_regen_rate: u8,
+    creature_generation_rate: u8,
+}
+
+impl World {
+    fn new(width: usize, height: usize, border: Tile, settings: WorldSettings) -> Self {
+        let mut tiles = vec![Tile::default(); width * height];
+
+        for x in 0..width {
+            tiles[x] = border.clone();
+            tiles[width * (height - 1) + x] = border.clone();
+        }
+
+        for y in 1..height - 1 {
+            tiles[y * width] = border.clone();
+            tiles[y * width + width - 1] = border.clone();
+        }
+
+        let creatures = HashMap::new();
+        let current_tick = 0;
+
+        World {
+            width,
+            height,
+            tiles,
+            creatures,
+            current_tick,
+            settings,
+        }
+    }
+
+    fn get_tile(&self, position: &Position) -> Option<&Tile> {
+        if self.check_bounds(position) {
+            Some(&self.tiles[position.y * self.width + position.x])
+        } else {
+            None
+        }
+    }
+
+    fn check_bounds(&self, position: &Position) -> bool {
+        position.x < self.width && position.y < self.height
+    }
+
+    fn tick(&mut self) {
+        self.current_tick += 1;
+        let actions = self
+            .creatures
+            .par_iter()
+            .filter_map(|(position, creature)| {
+                if let Some(brain) = &creature.brain {
+                    let mut neural_tick = NeuralTick::seed(brain, creature, position, &self);
+                    let action = neural_tick.calculate_action(brain);
+
+                    Some((position.clone(), action))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+        // for (position, creature) in self.creatures.iter() {
+        //     if let Some(brain) = &creature.brain {
+        //         let mut neural_tick = NeuralTick::seed(brain, creature, position, &self);
+        //         let action = neural_tick.calculate_action(brain);
+        //         actions.push((position.clone(), action));
+        //     } else {
+        //         actions.push((position.clone(), Action::Idle));
+        //     }
+        // }
+
+        for (position, action) in actions {
+            self.apply_action(&position, action);
+        }
+
+        for _ in 0..self.settings.creature_generation_rate {
+            self.randomize_creature();
+        }
+
+        for _ in 0..self.settings.food_regen_rate {
+            self.regenerate_food();
+        }
+    }
+
+    fn apply_action(&mut self, position: &Position, action: Action) {
+        {
+            let creature = self
+                .creatures
+                .get_mut(position)
+                .expect("An action needs to execute on a position with a creature");
+
+            let energy_cost = action.energy_cost();
+            if creature.energy >= energy_cost {
+                creature.energy = creature.energy.saturating_sub(action.energy_cost());
+            } else {
+                self.kill_creature(position);
+                return;
+            }
+
+            match action {
+                Action::Idle => (),
+                Action::Eat => {
+                    let tile = &mut self.tiles[position.x + position.y * self.width];
+                    if let Tile::Ground(data) = tile {
+                        if data.food_1 {
+                            data.food_1 = false;
+                            creature.energy += 50;
+                            if creature.energy > 150 {
+                                println!("Creature ate food! Now {}", creature.energy);
+                            }
+                        }
+                    }
+                }
+                Action::Move(location) => {
+                    let new_position = creature.relative_position(position, location);
+                    if self.check_bounds(&new_position) {
+                        self.move_creature(position, new_position);
+                    }
+                }
+                Action::Rotate(rotation) => {
+                    creature.rotation.rotate(rotation);
+                }
+                Action::CreateMembrane(location) => {
+                    let spawn_position = creature.relative_position(position, location);
+                    let rotation = creature.rotation.clone();
+                    self.spawn_creature(spawn_position, rotation, None);
+                }
+                Action::CopyDna(location) => {
+                    let copy_position = creature.relative_position(position, location);
+                    self.copy_dna(position, &copy_position);
+                }
+            }
+        }
+    }
+
+    fn move_creature(&mut self, old_position: &Position, new_position: Position) {
+        if self.creatures.contains_key(&new_position) {
+            return;
+        }
+        if let Some(creature) = self.creatures.remove(&old_position) {
+            let tile = self
+                .get_tile(&new_position)
+                .expect("Coordinate should be correct");
+            if !tile.can_contain_creature() {
+                // println!("Creature died to terrain");
+                return;
+            }
+            self.creatures.insert(new_position, creature);
+        }
+    }
+
+    fn spawn_creature(
+        &mut self,
+        position: Position,
+        rotation: CardinalDirection,
+        brain: Option<Arc<NeuralNetwork>>,
+    ) {
+        let tile = self
+            .get_tile(&position)
+            .expect("Coordinate should be correct");
+        if !tile.can_contain_creature() {
+            return;
+        }
+        self.creatures.entry(position).or_insert_with(|| {
+            let creature = Creature {
+                born: self.current_tick,
+                energy: Action::CreateMembrane(Location::InFront).energy_cost(),
+                brain,
+                rotation: rotation,
+            };
+            creature
+        });
+    }
+
+    fn copy_dna(&mut self, old_position: &Position, new_position: &Position) {
+        let [source, destination] = self
+            .creatures
+            .get_disjoint_mut([old_position, new_position]);
+        if let Some((source, destination)) = source.zip(destination) {
+            destination.brain = source.brain.clone();
+            println!("Creature DNA copied");
+        }
+    }
+
+    fn kill_creature(&mut self, position: &Position) {
+        self.creatures.remove(position);
+    }
+
+    fn randomize_creature(&mut self) {
+        let position = Position::randomize(self.width, self.height);
+        let rotation = CardinalDirection::randomize();
+        let brain = Some(Arc::new(NeuralNetwork::randomize()));
+        self.spawn_creature(position, rotation, brain);
+    }
+
+    fn regenerate_food(&mut self) {
+        let position = Position::randomize(self.width, self.height);
+        match &mut self.tiles[position.x + position.y * self.width] {
+            Tile::Ground(data) => {
+                data.food_1 = true;
+            }
+            Tile::Lava => {}
+        }
+    }
+}
+
+#[derive(Clone)]
+pub enum Tile {
+    Ground(AccessableTileData),
+    Lava,
+}
+
+impl Tile {
+    fn default() -> Self {
+        Tile::Ground(AccessableTileData::default())
+    }
+
+    fn can_contain_creature(&self) -> bool {
+        match self {
+            Tile::Ground(_) => true,
+            Tile::Lava => false,
+        }
+    }
+
+    fn color(&self) -> Color {
+        match self {
+            Tile::Ground(data) => data.color(),
+            Tile::Lava => Color {
+                r: 255,
+                g: 128,
+                b: 0,
+            },
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct AccessableTileData {
+    food_1: bool,
+}
+
+impl AccessableTileData {
+    fn default() -> Self {
+        AccessableTileData { food_1: false }
+    }
+
+    fn color(&self) -> Color {
+        if self.food_1 {
+            Color { r: 0, g: 255, b: 0 }
+        } else {
+            Color { r: 0, g: 0, b: 0 }
+        }
+    }
+}
+
+#[derive(PartialEq)]
+pub struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+impl Color {
+    fn randomize() -> Color {
+        Color {
+            r: fastrand::u8(0..255),
+            g: fastrand::u8(0..255),
+            b: fastrand::u8(0..255),
+        }
+    }
+}
+
+impl PartialOrd for Color {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        if self.r > other.r && self.g > other.g && self.b > other.b {
+            Some(std::cmp::Ordering::Greater)
+        } else {
+            None
+        }
+    }
+}
+
+pub struct Creature {
+    born: u64,
+    energy: u16,
+    rotation: CardinalDirection,
+    brain: Option<Arc<NeuralNetwork>>,
+}
+impl Creature {
+    fn relative_position(&self, position: &Position, location: Location) -> Position {
+        let cardinal = location.to_cardinal(&self.rotation);
+        position.cardinal(cardinal, 1)
+    }
+}
+
+#[derive(Clone)]
+pub enum CardinalDirection {
+    North,
+    East,
+    South,
+    West,
+}
+
+impl CardinalDirection {
+    fn rotate(&mut self, rotation: neural_network::Rotation) {
+        match rotation {
+            neural_network::Rotation::Clockwise => {
+                *self = match self {
+                    CardinalDirection::North => CardinalDirection::East,
+                    CardinalDirection::East => CardinalDirection::South,
+                    CardinalDirection::South => CardinalDirection::West,
+                    CardinalDirection::West => CardinalDirection::North,
+                };
+            }
+            neural_network::Rotation::CounterClockwise => {
+                *self = match self {
+                    CardinalDirection::North => CardinalDirection::West,
+                    CardinalDirection::East => CardinalDirection::North,
+                    CardinalDirection::South => CardinalDirection::East,
+                    CardinalDirection::West => CardinalDirection::South,
+                };
+            }
+        }
+    }
+
+    fn randomize() -> CardinalDirection {
+        match fastrand::u8(0..4) {
+            0 => CardinalDirection::North,
+            1 => CardinalDirection::East,
+            2 => CardinalDirection::South,
+            _ => CardinalDirection::West,
+        }
+    }
+}
---
+++ src/neural_network.rs
@@ -0,0 +1,303 @@
+use std::collections::HashSet;
+
+use arrayvec::ArrayVec;
+
+use crate::{CardinalDirection, Color, Creature, Position, World};
+
+#[derive(Clone)]
+pub enum Rotation {
+    Clockwise,
+    CounterClockwise,
+}
+impl Rotation {
+    fn randomize() -> Rotation {
+        match fastrand::bool() {
+            true => Rotation::Clockwise,
+            false => Rotation::CounterClockwise,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub enum Action {
+    Idle,
+    Move(Location),
+    Rotate(Rotation),
+    Eat,
+    CreateMembrane(Location),
+    CopyDna(Location),
+}
+
+impl Action {
+    pub fn energy_cost(&self) -> u16 {
+        match self {
+            Action::Idle => 1,
+            Action::Move(_) => 3,
+            Action::Rotate(_) => 2,
+            Action::Eat => 2,
+            Action::CreateMembrane(_) => 100,
+            Action::CopyDna(_) => 10,
+        }
+    }
+}
+
+const NEURON_COUNT: usize = 16;
+const MIN_GENERATED_NEURONS: usize = 6;
+const CONNECTION_COUNT: usize = 16;
+
+pub struct NeuralNetwork {
+    neurons: ArrayVec<Neuron, NEURON_COUNT>,
+    connections: ArrayVec<NeuralConnection, CONNECTION_COUNT>,
+}
+
+impl NeuralNetwork {
+    pub(crate) fn randomize() -> NeuralNetwork {
+        let neuron_count = fastrand::usize(MIN_GENERATED_NEURONS..=NEURON_COUNT);
+        let mut neurons = ArrayVec::new();
+        for _ in 0..neuron_count {
+            let neuron = Neuron::randomize();
+            neurons.push(neuron);
+        }
+
+        let input_neurons = neurons
+            .iter()
+            .enumerate()
+            .filter_map(|(index, neuron)| {
+                if neuron.has_input() {
+                    Some(index)
+                } else {
+                    None
+                }
+            })
+            .collect::<ArrayVec<_, NEURON_COUNT>>();
+
+        let output_neurons = neurons
+            .iter()
+            .enumerate()
+            .filter_map(|(index, neuron)| {
+                if neuron.has_output() {
+                    Some(index)
+                } else {
+                    None
+                }
+            })
+            .collect::<ArrayVec<_, NEURON_COUNT>>();
+
+        if output_neurons.is_empty() || input_neurons.is_empty() {
+            return Self::randomize();
+        }
+
+        let mut connections = HashSet::new();
+        let min_tries = neuron_count - 1;
+        let connection_generation_tries = fastrand::usize(min_tries..=CONNECTION_COUNT);
+        for _ in 0..connection_generation_tries {
+            let source = fastrand::u8(0..input_neurons.len() as u8);
+            let destination = fastrand::u8(0..output_neurons.len() as u8);
+            if input_neurons[source as usize] == output_neurons[destination as usize] {
+                continue;
+            }
+            connections.insert(NeuralConnection {
+                source,
+                destination,
+            });
+        }
+
+        Self {
+            neurons,
+            connections: connections.into_iter().collect(),
+        }
+    }
+}
+
+#[derive(Hash, Eq, PartialEq)]
+pub struct NeuralConnection {
+    source: u8,
+    destination: u8,
+}
+
+pub enum Neuron {
+    Input(InputNeuron),
+    Output(Action),
+}
+
+impl Neuron {
+    fn randomize() -> Self {
+        let neuron_type = fastrand::u8(0..=9);
+        match neuron_type {
+            0 => Self::Input(InputNeuron::AlwaysActive),
+            1 => Self::Input(InputNeuron::Random),
+            2 => {
+                let location = Location::randomize();
+                Self::Input(InputNeuron::Feeler(location))
+            }
+            3 => {
+                let location = match fastrand::u8(0..5) {
+                    0 => Some(Location::InFront),
+                    1 => Some(Location::Left),
+                    2 => Some(Location::Right),
+                    3 => Some(Location::Behind),
+                    4 => None,
+                    _ => unreachable!(),
+                };
+
+                let color = Color::randomize();
+                Self::Input(InputNeuron::Eye(location, color))
+            }
+            4 => Self::Output(Action::Idle),
+            5 => Self::Output(Action::Eat),
+            6 => {
+                let location = Location::randomize();
+                Self::Output(Action::Move(location))
+            }
+            7 => {
+                let rotation = Rotation::randomize();
+                Self::Output(Action::Rotate(rotation))
+            }
+            8 => {
+                let location = Location::randomize();
+                Self::Output(Action::CreateMembrane(location))
+            }
+            9 => {
+                let location = Location::randomize();
+                Self::Output(Action::CopyDna(location))
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    fn has_output(&self) -> bool {
+        match self {
+            Neuron::Input(_) => true,
+            Neuron::Output(_) => false,
+        }
+    }
+
+    fn has_input(&self) -> bool {
+        match self {
+            Neuron::Input(_) => false,
+            Neuron::Output(_) => true,
+        }
+    }
+}
+
+pub enum InputNeuron {
+    AlwaysActive,
+    Random,
+    Feeler(Location),
+    Eye(Option<Location>, Color),
+}
+
+#[derive(Clone)]
+pub enum Location {
+    InFront,
+    Left,
+    Right,
+    Behind,
+}
+
+impl Location {
+    pub fn to_cardinal(&self, look_direction: &CardinalDirection) -> CardinalDirection {
+        match (self, look_direction) {
+            (Location::InFront, cardinal) => cardinal.clone(),
+            (Location::Left, CardinalDirection::North) => CardinalDirection::West,
+            (Location::Left, CardinalDirection::West) => CardinalDirection::South,
+            (Location::Left, CardinalDirection::South) => CardinalDirection::East,
+            (Location::Left, CardinalDirection::East) => CardinalDirection::North,
+            (Location::Right, CardinalDirection::North) => CardinalDirection::East,
+            (Location::Right, CardinalDirection::East) => CardinalDirection::South,
+            (Location::Right, CardinalDirection::South) => CardinalDirection::West,
+            (Location::Right, CardinalDirection::West) => CardinalDirection::North,
+            (Location::Behind, CardinalDirection::North) => CardinalDirection::South,
+            (Location::Behind, CardinalDirection::South) => CardinalDirection::North,
+            (Location::Behind, CardinalDirection::West) => CardinalDirection::East,
+            (Location::Behind, CardinalDirection::East) => CardinalDirection::West,
+        }
+    }
+
+    fn randomize() -> Self {
+        match fastrand::u8(0..4) {
+            0 => Location::InFront,
+            1 => Location::Left,
+            2 => Location::Right,
+            _ => Location::Behind,
+        }
+    }
+}
+
+pub struct NeuralTick {
+    neuron_states: ArrayVec<NeuronValue, NEURON_COUNT>,
+}
+
+impl NeuralTick {
+    pub fn seed(net: &NeuralNetwork, me: &Creature, position: &Position, world: &World) -> Self {
+        let mut neuron_states = ArrayVec::new();
+        for neuron in &net.neurons {
+            let initial_output = match neuron {
+                Neuron::Input(input_neuron) => match input_neuron {
+                    InputNeuron::AlwaysActive => 1.0,
+                    InputNeuron::Random => fastrand::f32(),
+                    InputNeuron::Feeler(feel_location) => {
+                        let feel_position = me.relative_position(position, feel_location.clone());
+
+                        if world.creatures.contains_key(&feel_position) {
+                            1.0
+                        } else {
+                            0.0
+                        }
+                    }
+                    InputNeuron::Eye(look_location, color) => {
+                        let look_position = if let Some(location) = look_location {
+                            me.relative_position(position, location.clone())
+                        } else {
+                            position.clone()
+                        };
+
+                        if let Some(tile) = world.get_tile(&look_position) {
+                            if &tile.color() >= color { 1.0 } else { 0.0 }
+                        } else {
+                            0.0
+                        }
+                    }
+                },
+                Neuron::Output(_action) => 0.0,
+            };
+
+            neuron_states.push(NeuronValue {
+                input: 0.0,
+                output: initial_output,
+            });
+        }
+
+        Self { neuron_states }
+    }
+
+    pub fn calculate_action(&mut self, net: &NeuralNetwork) -> Action {
+        for connection in &net.connections {
+            let source_value = self.neuron_states[connection.source as usize].output;
+            self.neuron_states[connection.destination as usize].input += source_value;
+        }
+
+        let mut action = Action::Idle;
+        let mut action_impulse = 0.0;
+
+        for i in 0..self.neuron_states.len() {
+            match &net.neurons[i] {
+                Neuron::Input(_) => (),
+                Neuron::Output(neuron_action) => {
+                    let neuron_impulse = self.neuron_states[i].input;
+                    if neuron_impulse > action_impulse {
+                        action = neuron_action.clone();
+                        action_impulse = neuron_impulse;
+                    }
+                }
+            }
+        }
+
+        action
+    }
+}
+
+pub struct NeuronValue {
+    input: f32,
+    output: f32,
+}
